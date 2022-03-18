8d29e5a4a7f1e7abf501922d157439075476bf63
Luiz Cartolano <luiz.eduardo@bynd.com.br>
2022-03-18 19:02:38 -0300

    add open weather class with tests to make sure it's working

--- app.py
+++ app.py
@@ -3,10 +3,21 @@
 The app module implements the basic
 routes off application.
 """
-from flask import Flask, request
+from flask import Flask
+from flask_caching import Cache
+from src import CACHE_TTL
 
+
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": CACHE_TTL * 60
+}
 
 app = Flask(__name__)
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/ping', methods=['GET'])
@@ -20,3 +31,14 @@ def debug():
     }
 
     return test, 200
+
+
+@app.route('/temperature/<string:city_name>', methods=['GET'])
+@cache.cached(timeout=CACHE_TTL * 60, query_string=True)
+def get_temperature(city_name: str):
+    """
+    Method to return the temperature for a given city.
+
+    :return:
+    """
+    print(city_name)
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 """ Docstring for the __init__.py module.
 
 """
+from .constants import CACHE_TTL, DEFAULT_MAX_NUMBER
