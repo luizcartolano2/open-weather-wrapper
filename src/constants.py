95d103100f9e055ec4a8105264c0c688ec0b7d01
Luiz Cartolano <luiz.eduardo@bynd.com.br>
2022-03-18 17:27:31 -0300

    add environment variables with default value

--- src/constants.py
+++ src/constants.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-CACHE_TTL = getenv('CACHE_TTL', default=5)
-DEFAULT_MAX_NUMBER = getenv('DEFAULT_MAX_NUMBER', default=5)
+CACHE_TTL = int(getenv('CACHE_TTL', default='5'))
+DEFAULT_MAX_NUMBER = int(getenv('DEFAULT_MAX_NUMBER', default='5'))
